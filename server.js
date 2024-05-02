f2afff60a36589dcfa20f03f0965ca392f08b10c
DAE94 <david.alcaraz@institutvidreres.cat>
2024-05-02 11:57:47 +0200

    errg

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mysql = require('mysql2');
-const
+const {initModels} = require(".//models/init-models");
 
 
 app.use(cors());
